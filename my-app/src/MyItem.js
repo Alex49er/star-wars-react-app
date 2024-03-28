9c9acb9198ee5ac0ff2f9cb6f3eb3a6ccf37c397
Alex49er <151443937+Alex49er@users.noreply.github.com>
2024-03-28 15:33:12 +0000

    added and tested onclick event for component

--- my-app/src/MyItem.js
+++ my-app/src/MyItem.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 class Item extends React.Component{
+
+    clickMe(){
+        console.log("i clicked", this.props.name)
+    }
     render(){
       return(
-        <h1>Hello world</h1>
+        <h1 onClick={() => this.clickMe()}>Hi there, my name is {this.props.name}</h1>
       )
     }
 }
